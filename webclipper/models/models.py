654f1f73d8834f83012a1271039155c41289868d
Keyvan Mosharraf <github@keyvanm.com>
2022-04-06 13:10:07 -0700

    add uuid to WebClip

--- webclipper/models/models.py
+++ webclipper/models/models.py
@@ -1,10 +1,14 @@
+from building_blocks.models import HasUUID
 from django.conf import settings
 from django.db import models
 
 from .abstracts import AbstractWebClip
 
 
-class WebClip(AbstractWebClip):
+class WebClip(
+    HasUUID,
+    AbstractWebClip
+):
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, null=True, blank=True)
 
 
